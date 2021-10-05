ce48f0624c3cbbba1621537ac9ab204e68f3f392
Adam Foster <adam.foster@student.manchester.ac.uk>
2021-10-05 16:49:11 +0100

    End of tuesday

---
+++ fits_loading.py
@@ -0,0 +1,21 @@
+from astropy.io import fits
+import matplotlib.pyplot as plt
+import glob
+import os
+import numpy as np
+
+if __name__ == '__main__':
+
+    dir_name='J000fitstest' #Sets the name of the file the input png's are stored in
+
+    print('\n Begin \n')
+    imgs = {}
+    for filename in glob.iglob(os.getcwd() + '/' + f'{dir_name}' + '/**/*.fits', recursive=True): #operates over all png's within the desired directory
+
+        image_data = fits.getdata(filename)
+
+        print((np.transpose(image_data)).shape)
+
+        plt.imshow((np.transpose(image_data)*255).astype('uint8'))
+        plt.savefig(f'{filename}_from_fits_data.png')
+        plt.close()
